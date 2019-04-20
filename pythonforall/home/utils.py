9dab1545c7196208a43c8cf00272581db6c1924b
sammyt9 <sammy9@vt.edu>
2019-04-20 14:34:58 -0400

    better webscraper with continuous scrolling

--- pythonforall/home/utils.py
+++ pythonforall/home/utils.py
@@ -1,9 +1,26 @@
 import requests
+import time
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 def scrape():
-    page = requests.get('https://medium.com/search?q=python')
-    soup = BeautifulSoup(page.text, 'html.parser')
+    # browser = webdriver.Firefox()
+    browser = webdriver.Chrome('C:\\Users\\stien\\Downloads\\chromedriver_win32\\chromedriver.exe')
+    browser.get('https://medium.com/search?q=python')
+    time.sleep(1)
+
+    elem = browser.find_element_by_tag_name("body")
+
+    num_Pagedowns = 50
+
+    while num_Pagedowns:
+        elem.send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.3)
+        num_Pagedowns -= 1
+
+    page = browser.page_source
+    soup = BeautifulSoup(browser.page_source)
     body = soup.find(class_='col u-size9of12 u-sm-size12of12')
 
     articles = soup.findAll("div", {"class": "postArticle-content"})
